15103c4f8d30d3d66e2c5955e507e6e3f81783f8
richardjopia95 <richardjopia95@gmail.com>
2026-02-15 04:31:13 +0800

    commit

--- AccManagementJopia/Program.cs
+++ AccManagementJopia/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#include <iostream>
+   #include <string>
+   using namespace std;
+   
+   void check (string first, string last, string pw) {
+   string credentials;
+   credentials = first + last + pw;
+   
+   if (credentials == "RowellMarquinapogi") {
+   cout << "Welcome, Sir Well!";
+   }
+
+   else if (credentials == "ClyzaGananiloveNolan"){
+   cout << "Welcome, Mam Clyza!";
+   }
+
+   else if (credentials == "Rafael AntonioDeePogisiSir"){
+   cout << "Welcome, Master!";
+   }
+
+  else {
+  cout << "Sino ka?";
+  }
+  } 
+
+   int main(){
+   string fname, lname, password;
+
+   cout << "Enter First Name: ";
+   getline (cin,fname);
+
+   cout << "Enter Last Name: ";
+  getline (cin,lname);
+
+   cout << "Enter Password: ";
+   getline (cin,password);
+
+   check(fname,lname,password); 
+ 
+
+   }
