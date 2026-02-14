31c7a4b110188dc7b40a80a300f2bafdaeab4fb1
richardjopia95 <richardjopia95@gmail.com>
2026-02-15 04:40:23 +0800

    2nd

--- AccManagementJopia/Program.cs
+++ AccManagementJopia/Program.cs
@@ -1,41 +1,9 @@
-﻿#include <iostream>
-   #include <string>
-   using namespace std;
-   
-   void check (string first, string last, string pw) {
-   string credentials;
-   credentials = first + last + pw;
-   
-   if (credentials == "RowellMarquinapogi") {
-   cout << "Welcome, Sir Well!";
-   }
+﻿using System;
 
-   else if (credentials == "ClyzaGananiloveNolan"){
-   cout << "Welcome, Mam Clyza!";
-   }
-
-   else if (credentials == "Rafael AntonioDeePogisiSir"){
-   cout << "Welcome, Master!";
-   }
-
-  else {
-  cout << "Sino ka?";
-  }
-  } 
-
-   int main(){
-   string fname, lname, password;
-
-   cout << "Enter First Name: ";
-   getline (cin,fname);
-
-   cout << "Enter Last Name: ";
-  getline (cin,lname);
-
-   cout << "Enter Password: ";
-   getline (cin,password);
-
-   check(fname,lname,password); 
- 
-
-   }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
